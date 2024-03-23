fd82ec2c1b570e138e8c6ed999fbfe2d55f8412a
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-03-23 20:34:41 +0700

    Lab 4

---
+++ Lab4/index.js
@@ -0,0 +1,61 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var add;
+add = function (n1, n2) {
+    return n1 + n2;
+};
+var user1 = new Person();
+// user1.name = 'Manu';
+user1.greet('Hi there - I am');
+console.log(user1);
+// Bai 3
+var Department = /** @class */ (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.createEmployee = function (name) {
+        return { name: name };
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    Department.fiscalYear = 2020;
+    return Department;
+}());
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, 'IT') || this;
+        _this.admins = admins;
+        return _this;
+    }
+    ITDepartment.prototype.describe = function () {
+        console.log('IT Department - ID: ' + this.id);
+    };
+    return ITDepartment;
+}(Department));
+var employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
+var it = new ITDepartment('d1', ['Max']);
+it.addEmployee('Max');
+it.addEmployee('Manu');
+// Bai 4
